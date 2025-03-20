416204534a8718ceda0c85a26eb2addfc8f064fb
Javier Murua <javierantoniomurua@gmail.com>
2025-03-20 11:47:25 -0300

    Add config to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sharp from 'sharp';
+import ghPages from 'astro-gh-pages'; // 🚀 Agrega esta integración
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: "https://JavierMurua.github.io", // 🔹 Reemplaza con tu usuario y repo
+  base: "/sitio-web-astro/", // 🔹 Nombre del repositorio
+  output: "static", // 🔹 Asegura que Astro genere archivos estáticos
+  integrations: [tailwind(), react(), ghPages()], // 🔹 Agrega ghPages() aquí
   assets: {
     addSharpInstructions: true,
     services: [
