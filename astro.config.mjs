fe865579001f212fa12083f44cd95639c7cb3607
Javier Murua <javierantoniomurua@gmail.com>
2025-03-20 12:11:08 -0300

    Eliminate import  ghPages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sharp from 'sharp';
-import ghPages from 'astro-gh-pages'; // 🚀 Agrega esta integración
 
 export default defineConfig({
   site: "https://JavierMurua.github.io", // 🔹 Reemplaza con tu usuario y repo
   base: "sitio-web-astro", // 🔹 Nombre del repositorio
   output: "static", // 🔹 Asegura que Astro genere archivos estáticos
-  integrations: [tailwind(), react(), ghPages()], // 🔹 Agrega ghPages() aquí
+  integrations: [tailwind(), react()],
   assets: {
     addSharpInstructions: true,
     services: [
