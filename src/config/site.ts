0c735f4b1ffd133cc3b3ba9803e2355e705a74d1
Javier Murua <javierantoniomurua@gmail.com>
2025-09-19 17:03:56 -0300

    refactor: restructure index page layout and components

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+// Datos generales del sitio
+export const siteConfig = {
+  brand: {
+    short: "Z&P",
+    name: "Zalazar & Paredes",
+    description:
+      "Defensa en salud y trabajo. Rosario, Alvear, Pueblo Esther, Villa Amelia, Roldán, Funes, Carcarañá y Santa Fe.",
+  },
+  legal:
+    "La información presentada tiene fines generales y no constituye asesoramiento jurídico.",
+};
