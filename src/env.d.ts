2feac466877d4bd170658ad788895353b7218f60
Javier Murua <javierantoniomurua@gmail.com>
2025-09-20 17:31:21 -0300

    Add Icon as component

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+// Le decimos a TS cómo tipar los imports de SVG
+declare module '*.svg' {
+  const metadata: import('astro').ImageMetadata;
+  export default metadata;
+}
