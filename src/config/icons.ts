02fe946c00bdf2510409e3bf121e780c0a148d22
Javier Murua <javierantoniomurua@gmail.com>
2025-10-04 11:23:59 -0300

    Upgrade Icons

--- src/config/icons.ts
+++ src/config/icons.ts
@@ -5,17 +5,49 @@ import timeRaw from "../assets/svg/time.svg?raw";
 import pointerRaw from "../assets/svg/placeholder-map-pointer-svgrepo-com.svg?raw";
 import emailRaw from "../assets/svg/email-8-svgrepo-com.svg?raw";
 import phoneRaw from "../assets/svg/phone.svg?raw";
+import pillRaw from "../assets/svg/pill-svgrepo-com.svg?raw";
+import wheelchairRaw from "../assets/svg/wheelchair-svgrepo-com.svg?raw";
+import brokenLegRaw from "../assets/svg/broken-leg-svgrepo-com.svg?raw";
+import carRaw from "../assets/svg/car-svgrepo-com.svg?raw";
+import carAltRaw from "../assets/svg/car-svgrepo-com2.svg?raw";
+import crutchesRaw from "../assets/svg/crutches-svgrepo-com.svg?raw";
+import dnaRaw from "../assets/svg/dna-svgrepo-com.svg?raw";
+import familyRaw from "../assets/svg/family-svgrepo-com.svg?raw";
+import gavelRaw from "../assets/svg/gavel-svgrepo-com.svg?raw";
+import handshakeSlashRaw from "../assets/svg/handshake-slash-svgrepo-com.svg?raw";
+import helmetRaw from "../assets/svg/helmet-svgrepo-com.svg?raw";
+import legRaw from "../assets/svg/leg-svgrepo-com.svg?raw";
+import mechanicalArmRaw from "../assets/svg/mechanical-arm-robotics-svgrepo-com.svg?raw";
+import technicianRaw from "../assets/svg/technician-with-helmet-svgrepo-com.svg?raw";
 
 export const icons = {
-  car: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2 14.803v6.447c0 .414.336.75.75.75h1.614a.75.75 0 0 0 .74-.627L5.5 19h13l.395 2.373a.75.75 0 0 0 .74.627h1.615a.75.75 0 0 0 .75-.75v-6.447a5.954 5.954 0 0 0-1-3.303l-.78-1.17a1.994 1.994 0 0 1-.178-.33h.994a.75.75 0 0 0 .671-.415l.25-.5A.75.75 0 0 0 21.287 8H19.6l-.31-1.546a2.5 2.5 0 0 0-1.885-1.944C15.943 4.17 14.141 4 12 4c-2.142 0-3.943.17-5.405.51a2.5 2.5 0 0 0-1.886 1.944L4.399 8H2.714a.75.75 0 0 0-.67 1.085l.25.5a.75.75 0 0 0 .67.415h.995a1.999 1.999 0 0 1-.178.33L3 11.5c-.652.978-1 2.127-1 3.303zm15.961-4.799a4 4 0 0 0 .34.997H5.699c.157-.315.271-.65.34-.997l.632-3.157a.5.5 0 0 1 .377-.39C8.346 6.157 10 6 12 6c2 0 3.654.156 4.952.458a.5.5 0 0 1 .378.389l.631 3.157zM5.5 16a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM20 14.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-  </svg>`,
-  whatsapp: whatsappRaw,
+  // salud
+  pill: pillRaw,
+  dna: dnaRaw,
   ribbon: ribbonRaw,
-  time: timeRaw,
-  pointer: pointerRaw,
-  email: emailRaw,
+  family: familyRaw,
+  wheelchair: wheelchairRaw,
+
+  // laboral
+  gavel: gavelRaw,
+  handshakeSlash: handshakeSlashRaw,
+  helmet: helmetRaw,
+  technician: technicianRaw,
+
+  // accidentes
+  car: carRaw,
+  carAlt: carAltRaw,
+  brokenLeg: brokenLegRaw,
+  leg: legRaw,
+  crutches: crutchesRaw,
+  mechanicalArm: mechanicalArmRaw,
+
+  // comunicación
+  whatsapp: whatsappRaw,
   phone: phoneRaw,
+  email: emailRaw,
+  pointer: pointerRaw,
+  time: timeRaw,
 } as const;
 
 export type IconName = keyof typeof icons;
